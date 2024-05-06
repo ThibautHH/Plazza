44c3822a1c68ed6ad69f1ff0c643a8bd39dedb9b
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-05-06 17:40:08 +0200

    feat(args) Add usage message and argument count validation

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,24 @@
 ** Program entrypoint
 */
 
-int main(int, const char * const *)
+#include <iostream>
+
+static const char USAGE[] = " [SPEED MULTIPLIER] [COOK COUNT] [RESTOCK TIME]\n\n"
+"Starts The Plazza with kitchens restocking every RESTOCK TIME milliseconds\n"
+"and having COOK COUNT cooks cooking at a speed of SPEED MULTIPLIER times the\n"
+"normal speed.\n";
+
+static std::ostream &printUsage(std::ostream &os, const char * const command)
 {
+    return os << "Usage: " << command << USAGE;
+}
+
+int main(const int argc, const char * const * const argv)
+{
+    if (argc != 4) {
+        printUsage(std::cerr, argv[0]);
+        return 84;
+    }
+
     return 0;
 }
