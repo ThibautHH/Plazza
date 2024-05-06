6242baa4a2a8418c7b4a7c25758d56beac1e9c4d
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-05-06 19:21:07 +0200

    feat(args) Add special behavior for non-positive restock delay

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,21 @@
 
 #include <chrono>
 #include <iostream>
+#include <optional>
 #include <sstream>
 #include <utility>
 
 static const char USAGE[] = " [SPEED MULTIPLIER] [COOK COUNT] [RESTOCK TIME]\n\n"
 "Starts The Plazza with kitchens restocking every RESTOCK TIME milliseconds\n"
 "and having COOK COUNT cooks cooking at a speed of SPEED MULTIPLIER times the\n"
-"normal speed.\n",
+"normal speed.\n\n"
+"If RESTOCK TIME is 0, the kitchens will never run out of ingredients.\n"
+"If RESTOCK TIME is negative, the kitchens will never restock.\n",
     INVALID_SPEED_MULTIPLIER[] = "Invalid speed multiplier",
     INVALID_COOK_COUNT[] = "Invalid cook count",
     INVALID_RESTOCK_DELAY[] = "Invalid restock delay";
+
+using namespace std::chrono_literals;
 
 static std::ostream &printUsage(std::ostream &os, const char * const command)
 {
@@ -48,6 +53,9 @@ int main(const int argc, const char * const * const argv)
     auto cookCount = parseArg<std::uint16_t>(argv[2], INVALID_COOK_COUNT);
     if (static_cast<std::int16_t>(cookCount) <= 0)
         throw std::runtime_error(INVALID_COOK_COUNT);
+    std::optional<std::chrono::milliseconds> restockDelay(parseArg<std::int64_t>(argv[3], INVALID_RESTOCK_DELAY));
+    if (restockDelay < 0ms)
+        restockDelay = std::nullopt;
 
     return 0;
 }
