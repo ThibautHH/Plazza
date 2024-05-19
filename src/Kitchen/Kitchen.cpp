09934a8d6e1e381bc177dc06dd7cab347837c02f
Neo-Diamons <romain.hure@epitech.eu>
2024-05-20 01:15:34 +0200

    feat(kitchen): add NB_INGREDIENTS to the enum.

--- src/Kitchen/Kitchen.cpp
+++ src/Kitchen/Kitchen.cpp
@@ -13,16 +13,15 @@ Kitchen::Kitchen(const uint16_t maxCooks, const uint16_t cookingTime, const uint
     for (uint16_t i = 0; i < _maxCooks; i++)
         _cooks.emplace_back(Cook(i + 1, _cookingTime), std::thread());
 
-    for (uint16_t i = 0; i < 9; i++)
+    for (uint16_t i = 0; i < NB_INGREDIENTS; i++) {
         _ingredients.emplace_back(std::make_shared<BalancingSemaphore>(5));
-
-    for (uint16_t i = 0; i < 9; i++)
         _threadsIngredients.emplace_back(
             [this, i]
             {
                 while (*_isRunning)
                     _ingredients[i]->release([] { std::this_thread::sleep_for(std::chrono::milliseconds(1000)); });
             });
+    }
 
     _cooksManager = std::thread(
         [this]
@@ -73,7 +72,7 @@ Kitchen::~Kitchen()
 
 bool Kitchen::orderPizza(const Pizza pizza)
 {
-    if (_maxCooks - _nbCooks.getValue() + _waitingPizzas.size() > _maxCooks * 2)
+    if (_maxCooks - _nbCooks.getValue() + _waitingPizzas.size() >= _maxCooks * 2)
         return false;
 
     _asWaitingPizzas.release();
@@ -88,7 +87,7 @@ std::ostream &operator<<(std::ostream &os, const Kitchen &kitchen)
     for (const auto &[cook, thread] : kitchen._cooks)
         os << "\n    " << cook;
     os << "\n  Ingredients:";
-    for (uint16_t i = 0; i < 9; i++)
+    for (uint16_t i = 0; i < NB_INGREDIENTS; i++)
         os << "\n    " << ingredientName.at(static_cast<Ingredient>(1 << i)) << ": "
            << kitchen._ingredients[i]->getValue();
 
